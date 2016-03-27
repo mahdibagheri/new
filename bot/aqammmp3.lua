bdee7a6ae3dd5991873efc52bd2d7cf0b239ec4a
mahdibagheri <150mahdi@gmail.com>
2016-03-27 12:57:20 -0700

    Update aqammmp3.lua

--- bot/aqammmp3.lua
+++ bot/aqammmp3.lua
@@ -52,10 +52,10 @@ end
 
 function msg_valid(msg)
   -- Don't process outgoing messages
-  --if msg.out then
-    --print('\27[36mNot valid: msg from us\27[39m')
-    --return false
-  --end
+  if msg.out then
+    print('\27[36mNot valid: msg from us\27[39m')
+    return false
+  end
 
   -- Before bot was started
   if msg.date < os.time() - 5 then
@@ -78,10 +78,10 @@ function msg_valid(msg)
     return false
   end
 
-  --if msg.from.id == our_id then
-    --print('\27[36mNot valid: Msg from our id\27[39m')
-    --return false
-  --end
+  if msg.from.id == our_id then
+    print('\27[36mNot valid: Msg from our id\27[39m')
+    return false
+  end
 
   if msg.to.type == 'encr_chat' then
     print('\27[36mNot valid: Encrypted chat\27[39m')
@@ -229,7 +229,6 @@ function create_config( )
     "all",
     "leave_ban",
 	"supergroup",
-        "plugins",
 	"whitelist",
 	"msg_checks",
 	"idd"
@@ -334,7 +333,7 @@ This command will send text to [group_id]
 *Only admins and sudo can use res, setowner, commands
 ]],
     help_text = [[
-
+    
 !help
 دستورات مدیرتی :
 
@@ -440,6 +439,7 @@ This command will send text to [group_id]
 ️
 ]],
 	help_text_super =[[
+	
 دستورات مدیریتی سوپر گروه:
 
 !info
